ec42c7453ce61da20daabb932485d13b04915553
Ahmed Esmail <ahmedalawi01@gmail.com>
2024-06-25 22:00:00 +0300

    Section wrapper, wrap about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,6 +11,8 @@ import { services } from "../constants";
 
 import { fadeIn, textVariant } from "../utils/motion";
 
+import { SectionWrapper } from "../hoc";
+
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -63,4 +65,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
