a293d805f357200d73489dc872a7d129178b642e
Ahmed Esmail <ahmedalawi01@gmail.com>
2024-06-30 18:34:04 +0300

    Starts works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
+import { styles } from "../style";
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Works
+export default Works;
